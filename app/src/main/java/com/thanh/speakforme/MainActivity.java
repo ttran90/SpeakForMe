6104fb7dc1dff022f0da2bf065ae530f6625a0fa
Thanh Tran <ttran90@gatech.edu>
2016-10-22 06:07:50 -0400

    Able to get the input string from speech

--- app/src/main/java/com/thanh/speakforme/MainActivity.java
+++ app/src/main/java/com/thanh/speakforme/MainActivity.java
@@ -1,16 +1,57 @@
 package com.thanh.speakforme;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends Activity{
+
+    private TextView outputText;
+    Button startButton;
+    Button stopButton;
+    String inputString;
+    private SpeechRecognizer speechRecognizer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = (Button)findViewById(R.id.start);
+        stopButton = (Button)findViewById(R.id.stop);
+        outputText = (TextView)findViewById(R.id.outputText);
+        inputString = "";
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stuff();
+            }
+        });
+    }
+
+    private void stuff(){
+        Intent recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        try {
+            startActivityForResult(recognizerIntent, 100);
+        } catch(Exception e) {
+            Toast.makeText(getApplicationContext(), "Something broke", Toast.LENGTH_LONG);
+        }
     }
 
     @Override
@@ -19,6 +60,24 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 100: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    inputString = result.get(0);
+                }
+                break;
+            }
+        }
+    }
+
+    
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
